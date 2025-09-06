19e411bd329bc2d1e16e9e5a28d4008be0d097c3
Faheem <faheemxxx456@gmail.com>
2025-09-06 13:18:34 +0530

    monsterstats

--- script.js
+++ script.js
@@ -139,7 +139,10 @@ function fightDragon(){
 }
 function goFight(){
 update(locations[3]);
+monsterHealth= monsters[fighting].health;
+monsterStats.style.display="block";
 }
+
 function attack(){
 
 }
